dabfe9ab0539d519986121e0fe424a282534a37c
materivas <matiensoriv@gmail.com>
2025-08-27 21:20:13 +0000

    feat: Ejercicios 11, 12 y 13 resueltos. Creé el archivo real_estate_res_groups.xml y agregué los grupos Manager y Vendedor de Propiedades con sus respectivos permisos.

--- odoo/auto/addons/1user_task_manager/__manifest__.py
+++ odoo/auto/addons/1user_task_manager/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Modulo Grupo C',
+    'name': 'Real Estate',
     'version': '1.0',
     'summary': 'Un módulo creado en forma de practica para aprender Odoo',
     'description': 'Este módulo es creado por el grupo c.',
@@ -8,6 +8,7 @@
     'depends': ['base'],  
     'data': [
         #'security/estate_property_security.xml',
+        'security/real_estate_res_groups.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
     ],
